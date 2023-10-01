6faa0d55d5262e0682df0d8d2b92e3b14836f280
Sagar Utekar <sagarutekar2366@gmail.com>
2023-10-01 08:36:55 +0530

    Update app.py

--- projects/python-mongo-todo/local/app.py
+++ projects/python-mongo-todo/local/app.py
@@ -11,7 +11,7 @@ db = client.camp2016    #Select the database
 todos = db.todo #Select the collection
 
 app = Flask(__name__)
-title = "TODO with Flask"
+title = "TODO APPLICATION with Flask"
 heading = "Flask Application for Todo List"
 #modify=ObjectId()
 
